9a7a27ccbb2e46874ee67a6a3c0d3b767b8acdef
fercolchero <fercolchero@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-21 15:39:09 +0000

    included digits argument
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/print.basta.R
+++ pkg/R/print.basta.R
@@ -1,5 +1,5 @@
 print.basta <-
-function(object){
+function(object, digits=3){
 	catcov    = paste(colnames(object$Za), collapse=", ")
 	if(length(colnames(object$Za))==1) catcov = "NULL"
 	concov    = paste(colnames(object$Zc), collapse=", ")
@@ -10,7 +10,11 @@ function(object){
 	cat(paste("Cat. covars  \t\t", catcov,"\n",collapse=""))
 	cat(paste("Cont. covars \t\t", concov,"\n",collapse=""))
 
+	cat("\nRuns:\n")
+	id.failed    = which(object$finished==0)
+	if(sum(object$finished)==length(object$finished)) cat("All simulations finished.\n") else if(length(id.failed)==1) cat(paste("Simulation number ", id.failed, " failed.\n", sep="")) else cat(paste("Simulations number ", paste(id.failed, collapse=", "), " failed.\n", sep=""))
+	
 	cat("\nCoefficients:\n")
-	print(signif(object$coefficients, 3))
+	print.default(signif(object$coefficients, digits=digits))
 }
 
