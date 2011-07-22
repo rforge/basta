93cbfab32cb463a8efbe357e055a65ecb32d83b7
fercolchero <fercolchero@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-22 05:33:28 +0000

    delete mx.fun.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mx.fun.R
+++
@@ -1,9 +0,0 @@
-mx.fun <-
-function(x,th, ga=0, idm=3, log=FALSE){
-	mx    = th[,3] + exp(th[,4] + th[,5]*x)
-	if(idm==3) mx = mx + exp(th[,1]-th[,2]*x)
-	mx    = mx * exp(ga)
-	if(log) mx = log(mx)
-	return(mx)
-}
-
