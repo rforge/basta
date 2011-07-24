c7936531a9921c62fe8c3cd00812585be0e0f6cb
fercolchero <fercolchero@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-24 15:09:30 +0000

    changed object argument to x
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@84 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/print.basta.R
+++ pkg/R/print.basta.R
@@ -1,28 +1,28 @@
 print.basta <-
-function(object, digits=3){
-	catcov    = paste(colnames(object$Za), collapse=", ")
-	if(length(colnames(object$Za))==1) catcov = "NULL"
-	concov    = paste(colnames(object$Zc), collapse=", ")
-	if(length(colnames(object$Zc))==1) concov = "NULL"
+function(x, digits=3){
+	catcov    = paste(colnames(x$Za), collapse=", ")
+	if(length(colnames(x$Za))==1) catcov = "NULL"
+	concov    = paste(colnames(x$Zc), collapse=", ")
+	if(length(colnames(x$Zc))==1) concov = "NULL"
 	cat("\nCall:\n")
-	cat(paste("Model        \t\t", object$ModelSpecs[1],"\n",collapse=""))
-	cat(paste("Prop. hazards\t\t", object$ModelSpecs[2],"\n",collapse=""))
+	cat(paste("Model        \t\t", x$ModelSpecs[1],"\n",collapse=""))
+	cat(paste("Prop. hazards\t\t", x$ModelSpecs[2],"\n",collapse=""))
 	cat(paste("Cat. covars  \t\t", catcov,"\n",collapse=""))
 	cat(paste("Cont. covars \t\t", concov,"\n",collapse=""))
 
 	cat("\nRuns:\n")
-	id.failed    = which(object$finished==0)
-	if(object$set['nsim'] == 1){
+	id.failed    = which(x$finished==0)
+	if(x$set['nsim'] == 1){
 		if(length(id.failed) == 0) cat("The simulation finished.\n") else cat("The simulation failed.\n")
 	} else {
-		if(sum(object$finished)==length(object$finished)) cat("All simulations finished.\n") else if(length(id.failed)==1) cat(paste("Simulation number ", id.failed, " failed.\n", sep="")) else cat(paste("Simulations number ", paste(id.failed[-length(id.failed)], collapse=", ")," and ", id.failed[length(id.failed)], " failed.\n", sep=""))
+		if(sum(x$finished)==length(x$finished)) cat("All simulations finished.\n") else if(length(id.failed)==1) cat(paste("Simulation number ", id.failed, " failed.\n", sep="")) else cat(paste("Simulations number ", paste(id.failed[-length(id.failed)], collapse=", ")," and ", id.failed[length(id.failed)], " failed.\n", sep=""))
 	}
 	
 	
 	cat("\nCoefficients:\n")
-	print.default(object$coefficients, digits=digits)
-	if(is.null(object$ModSel)){
-		if(object$set['nsim'] == 1) cat("\nConvergence calculations require more than one run.\nTo estimate potential scale reduction run at least two simulations.\n") else cat("\nWarning: Convergence not reached for some parameters (i.e. 'PotScaleReduc' values larger than 1.2).\nThese estimates should not be used for inference.\n")
+	print.default(x$coefficients, digits=digits)
+	if(is.null(x$ModSel)){
+		if(x$set['nsim'] == 1) cat("\nConvergence calculations require more than one run.\nTo estimate potential scale reduction run at least two simulations.\n") else cat("\nWarning: Convergence not reached for some parameters (i.e. 'PotScaleReduc' values larger than 1.2).\nThese estimates should not be used for inference.\n")
 	} 
 
 }
