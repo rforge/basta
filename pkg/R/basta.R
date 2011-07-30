b1b4f93dd4fc81ee79246882f1a926b46060338e
fercolchero <fercolchero@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-30 09:53:31 +0000

    changed argument 'Data' to 'object'
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@106 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/basta.R
+++ pkg/R/basta.R
@@ -1,3 +1,3 @@
 basta <-
-function(Data, ststart, stend,model = "SI", niter = 50000, burnin = 5001, thinning = 50, rptp = ststart, th.ini.pars = NULL, th.jumps = NULL, th.priors = NULL, Prop.Hazards = FALSE, ga.ini.pars = NULL, ga.jumps = NULL, ga.priors = NULL, nsim = 1, parallel = FALSE, ncpus = 2, lifetable=TRUE ) UseMethod("basta")
+function(object, ... ) UseMethod("basta")
 
