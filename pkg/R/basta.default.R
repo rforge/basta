94ee7e58cf5fc88862da92b2cb8cf5039716af8e
fercolchero <fercolchero@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-02 18:59:55 +0000

    added argument ...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@159 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/basta.default.R
+++ pkg/R/basta.default.R
@@ -1,5 +1,5 @@
 basta.default <-
-function(object, ststart, stend, model="SI", niter=50000, burnin=5001, thinning=50, rptp = ststart, th.ini.pars=NULL, th.jumps=NULL, th.priors=NULL, Prop.Hazards = FALSE, ga.ini.pars=NULL, ga.jumps=NULL, ga.priors=NULL, nsim=1, parallel=FALSE, ncpus=2, lifetable=TRUE, progr.plot=FALSE){
+function(object, ststart, stend, model="SI", niter=50000, burnin=5001, thinning=50, rptp = ststart, th.ini.pars=NULL, th.jumps=NULL, th.priors=NULL, Prop.Hazards = FALSE, ga.ini.pars=NULL, ga.jumps=NULL, ga.priors=NULL, nsim=1, parallel=FALSE, ncpus=2, lifetable=TRUE, progr.plot=FALSE, ...){
 
 	# 0. Load package msm:
 	require(msm)
