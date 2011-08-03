576264334d867ebae7a60dbcbd99a5328d4c55ef
fercolchero <fercolchero@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-03 16:11:02 +0000

    trying to fix it
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@169 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -2,7 +2,14 @@
 <!-- This is the project specific website template -->
 <!-- It can be changed as liked or replaced by other content -->
 
-<?xml version="1.0" encoding="UTF-8"?>
+<?php
+
+$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
+$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
+$themeroot='http://r-forge.r-project.org/themes/rforge/';
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
 <!DOCTYPE html
 	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
