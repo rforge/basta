167c5d87992f3a1ad0ce44a80dc2d75d0520f7ed
fercolchero <fercolchero@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-04 06:36:15 +0000

    trying to fix it
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@170 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -76,10 +76,11 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 				<img alt="Sooty tern banding" border="1" src="sootyBand.jpg">
 			</td>
 			<td width="20">&nbsp;</td>
-			<td width="300">
+			<td width="400">
 				<h2 style="font-family:verdana;font-size:28px;color:#84002E;letter-spacing:0.2em">BaSTA</h2>
 				<p style="font-family:verdana;font-size:16px;text-align:justify"><i>Bayesian inference on age-specific survival for capture-recapture-recovery data with censored and truncated records</i></p>
 				<p style="font-family:verdana;font-size:14px"><b>Authors:</b><br>Fernando Colchero <a href="mailto:Colchero@demogr.mpg.de" rel="nofollow" style="color:#84002E">Colchero@demogr.mpg.de</a><br>Owen R. Jones <a href="mailto:Colchero@demogr.mpg.de" rel="nofollow" style="color:#84002E">Jones@demogr.mpg.de</a><br>Maren Rebke <a href="mailto:Colchero@demogr.mpg.de" rel="nofollow" style="color:#84002E">Rebke@demogr.mpg.de</a></p>
+				<p style="font-family:verdana;font-size:14px"><b>Developed at:</b><br><a href="http://www.demogr.mpg.de" rel="nofollow" style="color:#84002E;font-family:verdana" target="_blank">Max Planck Institute for Demographic Research</a><br>(<i>Laboratory of Evolutionary Biodemography and Independent Research Group Modeling the Evolution of Aging</i>).</p>
 			</td>
 			<td align="right" valign="bottom" width="200">
 				<a href="http://r-forge.r-project.org/"><img height="30px" src="http://r-forge.r-project.org/themes/rforge/images/logo.png"" border="0" alt="R-Forge Logo" /> </a>
