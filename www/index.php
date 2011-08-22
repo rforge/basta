6689316b084de2e24cee6ec386a07b71b104faad
fercolchero <fercolchero@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-22 06:16:18 +0000

    minor changes to link buttons
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@225 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -21,9 +21,9 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
     <link rel="stylesheet" type="text/css" href="bastastyle.css" /> 
   </head>
 
-  <body style="background:#DBDBDB">
+  <body style="background:#787878">
     <a name="top"></a>
-    <table style="vertical-align:middle;background:#DBDBDB" width="100%" height="100%">
+    <table style="vertical-align:middle;background:#787878" width="100%" height="100%">
       <tbody>
         <tr align="center">
           <td>
@@ -53,7 +53,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
               </tbody>
             </table>
             <!-- 3.- LOCAL LINKS:-->
-            <table width="1050px" style="table-layout:fixed;background:#FFFFFF" cellpadding="10">
+            <table width="1045px" style="table-layout:fixed;background:#FFFFFF" cellpadding="10">
               <tbody>
                 <tr>
                   <td align="center" valign="top" width="250">
@@ -66,14 +66,14 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
                     <p style="font-size:16px;line-height:1.25"><b>Authors:</b><br><br>Fernando Colchero <a href="mailto:Colchero@demogr.mpg.de" rel="nofollow" style="color:#84002E">Colchero@demogr.mpg.de</a><br>Owen R. Jones <a href="mailto:Colchero@demogr.mpg.de" rel="nofollow" style="color:#84002E">Jones@demogr.mpg.de</a><br>Maren Rebke <a href="mailto:Colchero@demogr.mpg.de" rel="nofollow" style="color:#84002E">Rebke@demogr.mpg.de</a><br></p>
                     <p style="font-size:16px;line-height:1.25;text-align:justify"><br><b>Developed at:</b><br><br><a href="http://www.demogr.mpg.de" rel="nofollow" style="color:#84002E;font-family:verdana" target="_blank">Max Planck Institute for Demographic Research</a><br>(<i>Max Planck Independent Research Group Modeling the Evolution of Aging and Laboratory of Evolutionary Biodemography</i>).</p>
                   </td>
-                  <td width="200px"></td>
+                  <td width="150px"></td>
                 </tr>
               </tbody>
             </table>
             <!-- 4.- SECTIONS:-->
             <!-- 4.1- Summary:-->
             <a name="summary"></a>
-            <table  valign="middle" width="1050px" style="table-layout:fixed;background:#DBDBDB">
+            <table  valign="middle" width="1050px" style="table-layout:fixed;background:#787878">
               <tbody>
                 <tr class="title"><td colspan="2" class="section">Project summary</td></tr>
                 <tr style="background:#FFFFFF">
@@ -82,7 +82,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
                     <a href="#top" class="totop">Back to top</a>
                   </td>
                   <td class="main">
-                    <p><img width="400px" align="middle" src="kestrelGO.png?width=400px"><br><br></p>
+                    <p><img style="margin:0px;padding:0px;border:none" width="400px" align="middle" src="kestrelGO.png?width=400px"><br><br></p>
                     <p class="caption">Fig. 1. <b>BaSTA</b> model output for sex differences in Kestrel mortality using a Gompertz survival model (Rebke <i>et al. in prep</i>). The left panel shows posterior distributions for the two survival parameters while the right panel shows the resulting survival probabilities and the mortality rates for males and females.</p>
                   </td>
                 </tr>
@@ -90,7 +90,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
             </table>
             <!-- 4.2- Package:-->
             <a name="description"></a>
-            <table  valign="middle" width="1050px" style="table-layout:fixed;background:#DBDBDB">
+            <table  valign="middle" width="1050px" style="table-layout:fixed;background:#787878">
               <tbody>
                 <tr class="title"><td colspan="2" class="section">Package description</td></tr>
                 <tr style="background:#FFFFFF">
@@ -109,7 +109,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
                     <a href="#top" class="totop">Back to top</a>
                   </td>
                   <td style="vertical-align:top;padding:40px" width="420px">
-                    <img width="420px" align="middle" src="BastaGeneralChart.jpg?width=420px">
+                    <img style="margin:0px;padding:0px;border:none;width:420px" align="middle" src="BastaGeneralChart.jpg?width=420px">
                     <p class="caption">Fig. 2. <b>BaSTA</b> general package chart.</p><br>
                     <ul>
                       <li>Runs multiple simulations either in parallel (using package snowfall; Knaus 2010) or in series.</li>
@@ -130,7 +130,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
             </table>
             <!-- 4.3- Model:-->
             <a name="model"></a>
-            <table  valign="middle" width="1050px" style="table-layout:fixed;background:#DBDBDB">
+            <table  valign="middle" width="1050px" style="table-layout:fixed;background:#787878">
               <tbody>
                 <tr class="title"><td colspan="2" class="section">Model description</td></tr>
                 <tr style="background:#FFFFFF">
@@ -147,7 +147,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
             </table>
             <!-- 4.4- References:-->
             <a name="refs"></a>
-            <table  valign="middle" width="1050px" style="table-layout:fixed;background:#DBDBDB">
+            <table  valign="middle" width="1050px" style="table-layout:fixed;background:#787878">
               <tbody>
                 <tr class="title"><td colspan="2" class="section">References</td></tr>
                 <tr style="background:#FFFFFF">
