c270fe145d9682892327837cb44ee4b80d7453a2
fercolchero <fercolchero@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-04 08:49:46 +0000

    fixed minor bug
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@280 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/basta.default.R
+++ pkg/R/basta.default.R
@@ -81,15 +81,21 @@ function(object, ststart, stend, model="GO", Shape="simple", covar.str = "mixed"
 
 	# 2.2 object processing:
 	Ith.fun <- function(Z){
-		lu          = apply(Z, 2, function(x) length(unique(x)))
-		ru          = apply(Z, 2, range)
-		idcat       = which(lu==2 & apply(ru,2,sum)==1)
-		if(length(idcat)==0) idcat = NULL
-		idint       = which(lu==1)
-		if(length(idint)==0) idint = NULL
-		idcon       = which(lu>2)
-		if(length(idcon)==0) idcon = NULL
-	
+		if(!is.null(Z)){
+			lu          = apply(Z, 2, function(x) length(unique(x)))
+			ru          = apply(Z, 2, range)
+			idcat       = which(lu==2 & apply(ru,2,sum)==1)
+			if(length(idcat)==0) idcat = NULL
+			idint       = which(lu==1)
+			if(length(idint)==0) idint = NULL
+			idcon       = which(lu>2)
+			if(length(idcon)==0) idcon = NULL
+		} else {
+			idcat = NULL
+			idint = NULL
+			idcon = NULL
+		}
+		
 		return(list(int=idint, cat=idcat, cont=idcon))
 	}
 
@@ -232,6 +238,7 @@ function(object, ststart, stend, model="GO", Shape="simple", covar.str = "mixed"
 		Covars    = FALSE
 		Cat       = FALSE
 		Cont      = FALSE
+		Ith       = Ith.fun(NULL)
 	}
 	nza         = ncol(Za)
 	nzc         = ncol(Zc)
