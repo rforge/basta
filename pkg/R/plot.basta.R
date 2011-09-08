fd2a30c89020d249f74dd87d6380268d55c93d22
fercolchero <fercolchero@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-08 08:08:16 +0000

    modified code to match R coding conventions and basta.default
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@290 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.basta.R
+++ pkg/R/plot.basta.R
@@ -1,81 +1,143 @@
 plot.basta <-
-function(x, plot.trace=TRUE, tracename = "theta", ...){
-   	Palette    = c("#A6CEE3", "#1F78B4", "#B2DF8A", "#33A02C", "#FB9A99", "#E31A1C", "#FDBF6F", "#FF7F00", "#CAB2D6", "#6A3D9A", "#FFFF99", "#B15928")
+function(x, plot.trace=TRUE, trace.name = "theta", ...){
 
-	if(plot.trace){
-		plotname     = c("theta","gamma","pi", "post")
+  Palette           <- c("#A6CEE3", "#1F78B4", "#B2DF8A", "#33A02C", 
+                         "#FB9A99", "#E31A1C", "#FDBF6F", "#FF7F00", 
+                         "#CAB2D6", "#6A3D9A", "#FFFF99", "#B15928")
+  length.cat        <- ncol(x$Zcat)
+  if(plot.trace){
+  	# 1. Trace plots for parameters:
+    traces          <- c("theta","gamma","pi", "post")
+    if(!is.element(trace.name, traces)) {
+      stop(paste("Wrong 'trace.name' argument. Valid arguments are:", 
+      paste(paste("'", traces, "'", sep = ""), collapse = ", "), ".\n"), 
+      call. = FALSE)
+    }
+    varNames        <- substr(colnames(x$Par)[-1], 1, 2)
+    id.th           <- is.element(substr(varNames, 1, 1), c("a", "b", "c"))
+    varNames[id.th] <- "th"
+    nTraces         <- table(varNames)[c("th", "ga", "pi", "po")]
+    names(nTraces)  <- c("th", "ga", "pi", "po")
+    nTraces[is.na(nTraces)] <- 0
+    if (trace.name == "gamma" & nTraces["ga"] == 0) {
+      stop("\nTrace plots cannot be drawn for 'gamma' parameters.",
+           "\nNo proportional hazards arguments were evaluated.", 
+           call. = FALSE)
+    }
+    niter           <- x$settings['niter']
+    nsim            <- x$settings['nsim' ]
+    simNames        <- unique(rownames(x$Par))
+    idpl            <- which(varNames == substr(trace.name, 1, 2))
+    X               <- as.matrix(x$Par[, -1][, idpl])
+    colnames(X)     <- colnames(x$Par)[-1][idpl]
+    p               <- which(traces == trace.name)
+    Cols            <- Palette[round(seq(1, 12, length = nsim))]
+    model           <- as.character(x$ModelSpecs['model'])
+    shape           <- as.character(x$ModelSpecs['shape'])
+    if (model == "EX") {
+      nthm          <- 1
+    } else if (model == "GO" | model == "WE") {
+      nthm          <- 2
+    }  else {
+      nthm          <- 3
+    }
+    if (shape == "Makeham") {
+      nthm          <- nthm + 1
+    } else if(shape == "bathtub") {
+      nthm          <- nthm + 3
+    }
+    ydim            <- c(nthm, ceiling(nTraces['ga'] / 2), 
+                         ceiling(nTraces['pi'] / 2), 2)
+    xdim            <- c(length.cat, 2, 2, 2)
+    for(ii in 2:3) if (ydim[ii] == 1) xdim[ii] <- 1
+    op              <- par(mfrow = c(ydim[p], xdim[p]), 
+                           mar   = c(3, 3, 3, 1))
+    for(i in 1:nTraces[p]){
+      x             <- X[,i]
+      yl            <- range(x, na.rm = TRUE)
+      plot(x          = c(1,niter), 
+           y          = yl, 
+           col        = NA, 
+           xlab       = "Iteration", 
+           ylab       = "", 
+           main       = colnames(X)[i], 
+           frame.plot = FALSE)
+      for(j in 1:nsim) {
+        lines(x       = x[which(names(x) == simNames[j])], 
+              type    = 'l', 
+              col     = Cols[j], 
+              lwd     = 1.5)
+      }
+    }
+    par(op)
 
-		if(!is.element(tracename, plotname)) stop(paste("Wrong 'tracename' argument. Valid arguments are:", paste(paste("'",plotname,"'", sep=""), collapse=", "),".\n"), call.=FALSE)
-	
-		pnames       = substr(colnames(x$Par)[-1],1,2)
-		pnames[is.element(substr(pnames,1,1), c("a","b","c"))] = "th"
-		npars        = table(pnames)[c("th","ga","pi","po")]
-		names(npars) = c("th","ga","pi","po")
-		npars[is.na(npars)] = 0
-		if(tracename=="gamma" & npars["ga"]==0) stop("\nTrace plots cannot be drawn for 'gamma' parameters.\nNo proportional hazards arguments were evaluated.", call.=FALSE)
-	
+  # 2. Plot survival and mortality:
+  } else {
 
-		# PLOTS:
-		# 1. Trace plots for parameters:
-		ng           = x$settings['niter']
-		nsim         = x$settings['nsim' ]
-		nza          = ncol(x$Zcat)
-		simname      = unique(rownames(x$Par))
-		idpl         = which(pnames==substr(tracename,1,2))
-		X            = as.matrix(x$Par[,-1][,idpl])
-		colnames(X)  = colnames(x$Par)[-1][idpl]
-		p            = which(plotname == tracename)
-		Cols         = Palette[round(seq(1,12, length=nsim))]
-		model        = as.character(x$ModelSpecs['model'])
-		shape        = as.character(x$ModelSpecs['shape'])
-		if(model == "EX") nthm = 1 else if(model=="GO" | model=="WE") nthm = 2 else nthm = 3
-		if(shape=="Makeham") nthm = nthm + 1 else if(shape=="bathtub") nthm = nthm + 3
-		ydim         = c(nthm, ceiling(npars['ga']/2), ceiling(npars['pi']/2), 2)
-		xdim         = c(nza, 2, 2, 2)
-		for(ii in 2:3) if(ydim[ii]==1) xdim[ii] = 1
+    if(is.null(x$Sx)){
+      stop("MCMC runs on BaSTA did not finish.\n Survival and mortality ",
+           "plots cannot be constructed, verify model input and run again.\n",
+           call. = FALSE)
+    }
 
-		op           = par(mfrow=c(ydim[p], xdim[p]), mar=c(3,3,3,1))
-		for(i in 1:npars[p]){
-			x = X[,i]
-			yl          = range(x, na.rm=TRUE)
-			plot(c(1,ng), yl, col=NA, xlab="Iteration", ylab="", main=colnames(X)[i], frame.plot=FALSE)
-			for(j in 1:nsim) lines(x[which(names(x) == simname[j])], type='l', col=Cols[j], lwd=1.5)
-		}
-		par(op)
+    zname           <- names(x$Sx)
+    length.cat      <- length(zname)
+    Bord            <- Palette[round(seq(1, 12, length = length.cat))]
+    Cols            <- adjustcolor(Bord, alpha.f = 0.5)
+    op              <- par(mfrow = c(2, 1), mar = c(4, 4, 3, 2))
 
-	} else {
-		# 2. Plot survival and mortality:
-		
-		if(is.null(x$Sx)){
-			stop("MCMC runs on BaSTA did not finish.\n Survival and mortality plots cannot be constructed, verify model input and run again.\n", call.=FALSE)
-		}
+    # Plot survival probability:
+    mxv             <- ceiling(max(unlist(x$xv)) / 5) * 5
+    plot(x           = c(0, mxv), 
+         y           = range(0, 1), 
+         col         = NA, 
+         xlab        = "", 
+         ylab        = expression(S(x)), 
+         main        = "Survival probability", 
+         frame.plot  = FALSE, ...)
+    for(i in 1:length.cat){
+      xv            <- x$xv[[i]]
+      polygon(x      = c(xv, rev(xv)), 
+              y      = c(x$Sx[[i]][[1]][, 2, 1], rev(x$Sx[[i]][[1]][, 3, 1])), 
+              col    = Cols[i], 
+              border = Bord[i])
+      lines(x        = xv, 
+            y        = x$Sx[[i]][[1]][,1,1], 
+            col      = Bord[i], 
+            lty      = 3)
+    }
+    if (length.cat > 1) {
+      legend(x       = 'topright', 
+             legend  = zname, 
+             pch     = 15, 
+             pt.cex  = 3, 
+             cex     = 1.5, 
+             col     = Cols, 
+             bty     = 'n')
+    }
 
-		zname        = names(x$Sx)
-		nza          = length(zname)
-		Bord         = Palette[round(seq(1,12, length=nza))]
-		Cols         = adjustcolor(Bord, alpha.f=0.5)
-	
-		op           = par(mfrow=c(2,1), mar=c(4,4,3,2))
-
-		# Plot survival probability:
-		mxv          = ceiling(max(unlist(x$xv))/5)*5
-		plot(c(0,mxv), range(0,1), col=NA, xlab="", ylab=expression(S(x)), main="Survival probability", frame.plot=FALSE, ...)
-		for(i in 1:nza){
-			xv         = x$xv[[i]]
-			polygon(c(xv, rev(xv)), c(x$Sx[[i]][[1]][,2,1], rev(x$Sx[[i]][[1]][,3,1])), col=Cols[i], border=Bord[i])
-			lines(xv, x$Sx[[i]][[1]][,1,1], col=Bord[i], lty=3)
-		}
-		if(nza>1) legend('topright', zname, pch=15, pt.cex=3, cex=1.5, col=Cols, bty='n')
-
-		# Plot mortality rates:
-		ylmx         = c(0, round(max(unlist(x$mx))))
-		plot(c(0,mxv), ylmx, col=NA, xlab="Age (x)", ylab=expression(mu(x)), main="Mortality rate", frame.plot=FALSE, ylim=ylmx, ...)
-		for(i in 1:nza){
-			xv         = x$xv[[i]]
-			polygon(c(xv, rev(xv)), c(x$mx[[i]][[1]][,2,1], rev(x$mx[[i]][[1]][,3,1])), col=Cols[i], border=Bord[i])
-			lines(xv, x$mx[[i]][[1]][,1,1], col=Bord[i], lty=3)
-		} 
-		par(op)
-	}
+    # Plot mortality rates:
+    ylmx            <- c(0, round(max(unlist(x$mx))))
+    plot(x           = c(0,mxv), 
+         y           = ylmx, 
+         col         = NA, 
+         ylim        = ylmx,
+         xlab        = "Age (x)", 
+         ylab        = expression(mu(x)), 
+         main        = "Mortality rate", 
+         frame.plot  = FALSE, ...)
+    for(i in 1:length.cat){
+      xv            <- x$xv[[i]]
+      polygon(x      = c(xv, rev(xv)), 
+              y      = c(x$mx[[i]][[1]][,2,1], rev(x$mx[[i]][[1]][,3,1])), 
+              col    = Cols[i], 
+              border = Bord[i])
+      lines(x        = xv, 
+            y        = x$mx[[i]][[1]][, 1, 1], 
+            col      = Bord[i], 
+            lty      = 3)
+    }
+    par(op)
+  }
 }
 
