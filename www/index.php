fb050d0ee05ba75a25339e6e7de3148f0fa4ff50
fercolchero <fercolchero@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-18 10:50:22 +0000

    Added google analytitcs tracking code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@310 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -19,6 +19,19 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>BaSTA</title>
     <link rel="stylesheet" type="text/css" href="bastastyle.css" /> 
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-4964902-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+  </script>
   </head>
 
   <body style="background:#CCCCCC">
@@ -180,17 +193,4 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
       </tbody>
     </table>
   </body>
-	<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-4964902-4']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 </html>
