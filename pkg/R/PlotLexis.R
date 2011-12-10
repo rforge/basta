b8dfb880d199458964b779c77f5fec0bf0d54860
jonesor <jonesor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-10 15:32:48 +0000

    Added 1st draft of simple plot lexis function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@463 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/PlotLexis.R
@@ -0,0 +1,13 @@
+PlotLexis <- function(x,ci=1,ji=0.5,col="#007FFF15"){
+
+BirthTime = x$Bq[ci,]
+AgeAtDeath = x$Xq[ci,]
+DeathTime = BirthTime+AgeAtDeath
+
+plot(c(min(BirthTime),max(BirthTime)),c(0,max(AgeAtDeath)),type="n",axes=T,xlab="Year",ylab="Age (yrs)",xlim=c(min(BirthTime),max(DeathTime)))
+
+for (i in 1:length(BirthTime)){
+jitterval = runif(1,0,1)*ji
+lines(c(BirthTime[i]+jitterval,DeathTime[i]+jitterval),(c(0,AgeAtDeath[i])),col=col,lwd=4)
+}
+}
