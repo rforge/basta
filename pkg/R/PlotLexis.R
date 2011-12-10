87b200f9f56129c21b504ce08d27010ad26bef71
jonesor <jonesor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-10 22:03:42 +0000

    added functionality to include categorical covariates automatically in PlotLexis
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@466 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/PlotLexis.R
+++ pkg/R/PlotLexis.R
@@ -1,13 +1,21 @@
-PlotLexis <- function(x,ci=1,ji=0.5,col="#007FFF15",xlab="Year",ylab="Age (yrs)"){
+PlotLexis <- function(x,ci=1,ji=0.5,col=c("#007FFF15","#FF000015"),xlab="Year",ylab="Age (yrs)"){
 
-BirthTime = x$Bq[ci,]
+BirthTime  = x$Bq[ci,]
 AgeAtDeath = x$Xq[ci,]
-DeathTime = BirthTime+AgeAtDeath
+DeathTime  = BirthTime+AgeAtDeath
+nCat       = ncol(x$Zcat)
 
-plot(c(min(BirthTime),max(BirthTime)),c(0,max(AgeAtDeath)),type="n",axes=T,xlab=xlab,ylab=ylab,xlim=c(min(BirthTime),max(DeathTime)))
+par(mfrow=c(nCat,1))
+for(nc in 1:nCat){
+    plot(c(min(BirthTime),max(BirthTime)),c(0,max(AgeAtDeath)),type="n",axes=T,xlab=xlab,ylab=ylab,xlim=c(min(BirthTime),max(DeathTime)))
 
-for (i in 1:length(BirthTime)){
-jitterval = runif(1,0,1)*ji
-lines(c(BirthTime[i]+jitterval,DeathTime[i]+jitterval),(c(0,AgeAtDeath[i])),col=col,lwd=4)
-}
+    ssbt  = BirthTime[x$Zcat[,nc]==1]
+    ssdt  = DeathTime[x$Zcat[,nc]==1]
+    ssaad = ssdt-ssbt
+
+    for (i in 1:length(ssbt)){
+        jitterval = runif(1,0,1)*ji
+        lines(c(ssbt[i]+jitterval,ssdt[i]+jitterval),(c(0,ssaad[i])),col=col[nc],lwd=4)
+        }
+    }
 }
