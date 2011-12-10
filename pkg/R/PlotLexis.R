845f27c5afd7000cbc6318f515bf864f7021725e
jonesor <jonesor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-10 15:33:24 +0000

    Minor tweaks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@464 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/PlotLexis.R
+++ pkg/R/PlotLexis.R
@@ -1,10 +1,10 @@
-PlotLexis <- function(x,ci=1,ji=0.5,col="#007FFF15"){
+PlotLexis <- function(x,ci=1,ji=0.5,col="#007FFF15",xlab="Year",ylab="Age (yrs)"){
 
 BirthTime = x$Bq[ci,]
 AgeAtDeath = x$Xq[ci,]
 DeathTime = BirthTime+AgeAtDeath
 
-plot(c(min(BirthTime),max(BirthTime)),c(0,max(AgeAtDeath)),type="n",axes=T,xlab="Year",ylab="Age (yrs)",xlim=c(min(BirthTime),max(DeathTime)))
+plot(c(min(BirthTime),max(BirthTime)),c(0,max(AgeAtDeath)),type="n",axes=T,xlab=xlab,ylab=ylab,xlim=c(min(BirthTime),max(DeathTime)))
 
 for (i in 1:length(BirthTime)){
 jitterval = runif(1,0,1)*ji
