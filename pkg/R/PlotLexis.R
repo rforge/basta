b8ab88decf28a53d15731550a687daca1ffe0d30
jonesor <jonesor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-11 13:06:56 +0000

    Added elipsis, to pass arguments to plot. Added line width argument.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/basta@468 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/PlotLexis.R
+++ pkg/R/PlotLexis.R
@@ -1,4 +1,4 @@
-PlotLexis <- function(x,ci=1,ji=0.5,col=c("#FF000015","#007FFF15"),xlab="Year",ylab="Age (yrs)"){
+PlotLexis <- function(x,ci=1,ji=0.5,lw=3,col=c("#FF000015","#007FFF15"),...){
 
 BirthTime  = x$Bq[ci,]
 AgeAtDeath = x$Xq[ci,]
@@ -8,15 +8,19 @@ CatNames   = colnames(x$Zcat)
 
 par(mfrow=c(nCat,1))
 for(nc in 1:nCat){
-    plot(c(min(BirthTime),max(BirthTime)),c(0,max(AgeAtDeath)),type="n",axes=T,xlab=xlab,ylab=ylab,xlim=c(min(BirthTime),max(DeathTime)))
+    plot(c(min(BirthTime),max(BirthTime)),c(0,max(AgeAtDeath)),type="n",xlim=c(min(BirthTime),max(DeathTime)),...)
     title(main=CatNames[nc])
     ssbt  = BirthTime[x$Zcat[,nc]==1]
     ssdt  = DeathTime[x$Zcat[,nc]==1]
     ssaad = ssdt-ssbt
+    
+    #Grid
+    #abline(v=min(BirthTime):max(DeathTime),col="grey35")
+    #abline(h=1:max(ceiling(AgeAtDeath)),col="grey35")
 
     for (i in 1:length(ssbt)){
         jitterval = runif(1,0,1)*ji
-        lines(c(ssbt[i]+jitterval,ssdt[i]+jitterval),(c(0,ssaad[i])),col=col[nc],lwd=4)
+        lines(c(ssbt[i]+jitterval,ssdt[i]+jitterval),(c(0,ssaad[i])),col=col[nc],lwd=lw)
         }
     }
 }
